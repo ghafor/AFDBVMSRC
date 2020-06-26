1bb2ac651dbb955c1367bc4e6e24d0f789bedde2
jebran10 <44675727+jebran10@users.noreply.github.com>
2020-06-26 13:10:42 -0400

    units correction and cleanup

--- sensor_codes.ino
+++ sensor_codes.ino
@@ -1,27 +1,37 @@
 void setup() {
-  // put your setup code here, to run once:
-Serial.begin(9600);
+  	// initiating data rate of 9600 bps
+	Serial.begin(9600);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  read();
+  	// repeatedly calling read function
+  	read();
 }
 
 
 void read() {
     // read the voltage
-    int V = analogRead(A0); 
-    int Vs = analogRead(A1);
-    float Pmin = -2;   // pressure max in mbar
-    float Pmax = 2;    // pressure min in mbar
-    float Vmax = 1023;     // max voltage in range from analogRead
+
+    int V = analogRead(A0); 	// sensor Vout in (0-1023)
+    int Vs = analogRead(A1);	// sensor Vsupply in (0-1023)
+
+    float Pmin = -2;   			// pressure max in Psi
+    float Pmax = 2;    			// pressure min in Psi
+
+    float Vmax = 1023;     		// max voltage in range from analogRead
+
     // convert to pressure
-    float Vs_A = (5*Vs/Vmax);
-   float pres = (((5*V/Vmax) -0.5)*(Pmax - Pmin))/(0.8*Vs_A) + Pmin;//psi
-    pres *= 70.307; // cmH2O
-    // convert to cmH20
+    float Vs_A = (5*Vs/Vmax);	// sensor Vsupply in V
+    float Vout = (5*V/Vmax);	// sensor Vout in V
+
+    // pressure difference between port 1 and 2 of the sensor in Psi
+    // based on the TE Connectivity calibration curve for MS4525 002 sensor
+
+    float P = ((Vout - 0.5)*(Pmax - Pmin)/(0.8*Vs_A)) + Pmin;
+
+    // convert to cmH2O
+    P *= 70.307; 				// cmH2O (1 Psi = 70.307 cmH2O)
   
-    Serial.println(pres);
+    Serial.println(P);
     delay(200);
   }
