e8b5f531553770caf190f20d6cfbac7f2aaa078f
Ghafor Yaqubi <ghafor214@gmail.com>
2020-06-26 13:49:34 +0430

    Update sensor_codes.ino

--- sensor_codes.ino
+++ sensor_codes.ino
@@ -13,15 +13,15 @@ void read() {
     // read the voltage
     int V = analogRead(A0); 
     int Vs = analogRead(A1);
-    float Pmin = -2;   // pressure max in mbar
-    float Pmax = 2;    // pressure min in mbar
+    float Pmin = -1.4503773773;   // pressure max in mbar
+    float Pmax = 1.4503773773;    // pressure min in mbar
     float Vmax = 1023;     // max voltage in range from analogRead
     // convert to pressure
     float Vs_A = (5*Vs/Vmax);
-    float pres = ((5*V/1023) -0.5)*(Pmax - Pmin)/(0.8*Vs_A) + Pmin;//psi
+   float pres = (((5*V/Vmax) -0.5)*(Pmax - Pmin))/(0.8*Vs_A) + Pmin;//psi
     pres *= 70.307; // cmH2O
     // convert to cmH20
   
-    Serial.println(Vs_A);
+    Serial.println(pres);
     delay(200);
   }
